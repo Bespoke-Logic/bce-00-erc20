7844584c10c9504544de7e12233272e35a07fb2b
Edward Smith <edwardmsmith@gmail.com>
2022-03-17 17:13:39 -0500

    A few more tests just for practice.  I could test every function, but that would be redundant with OpenZeppelin tests.

--- test/sample-test.js
+++ test/sample-test.js
@@ -18,5 +18,21 @@ describe("Petrol", function () {
   it("Should have an initial quantity of 10M coins", async function () {
     const accounts = await hre.ethers.getSigners();
     expect(await petrolCoin.balanceOf(accounts[0].address)).to.equal(10000000);
+    expect(await petrolCoin.totalSupply()).to.equal(10000000);
+  });
+
+  it("Should support transfer", async function () {
+    const accounts = await hre.ethers.getSigners();
+    const owner = accounts[0].address;
+    const receiver = accounts[1].address;
+    expect(await petrolCoin.balanceOf(owner)).to.equal(10000000);
+    await petrolCoin.transfer(receiver, 10000);
+    expect(await petrolCoin.balanceOf(owner)).to.equal(10000000 - 10000);
+    expect(await petrolCoin.balanceOf(receiver)).to.equal(0 + 10000);
+    await expect(petrolCoin.transfer(receiver, 10000))
+      .to.emit(petrolCoin, "Transfer")
+      .withArgs(owner, receiver, 10000);
+    expect(await petrolCoin.balanceOf(owner)).to.equal(10000000 - 20000);
+    expect(await petrolCoin.balanceOf(receiver)).to.equal(0 + 20000);
   });
 });
